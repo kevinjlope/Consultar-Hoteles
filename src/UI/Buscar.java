c7fe08396ee4f19bee08143b490bf8bf3ffbddab
Kevin Lopez <kevinjeff04@gmail.com>
2020-04-04 13:19:56 -0500

    El UI buscar ya muestra en pantalla las ciudades, que consulta el usuario por provincia

--- src/UI/Buscar.java
+++ src/UI/Buscar.java
@@ -1,7 +1,7 @@
 package UI;
 
-
 import java.io.IOException;
+import java.util.ArrayList;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -9,61 +9,76 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import learninggitjava.Ciudad;
 import learninggitjava.Provincia;
+//import learninggitjava.Provincia;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author kevjlope
  */
 public class Buscar {
+
     private VBox rootPrincipal;
     private BorderPane rootGP;
     private ComboBox comboProvincia;
     private ComboBox comboCiudad;
-    
-    public VBox getRootPrincipal(){
+
+    public VBox getRootPrincipal() {
         return rootPrincipal;
     }
-    
-    public Buscar(){
+
+    public Buscar() {
         rootPrincipal = new VBox();
         rootPrincipal.getChildren().add(menuBusqueda());
     }
-    
-    private BorderPane menuBusqueda(){
+
+    private BorderPane menuBusqueda() {
         rootGP = new BorderPane();
-        
+
         rootGP.setTop(topBusqueda());
-       // rootGP.setTop(new Label("Escoja alguna provincia"));
+        // rootGP.setTop(new Label("Escoja alguna provincia"));
         return rootGP;
     }
-    
-    private AnchorPane topBusqueda(){
+
+    private AnchorPane topBusqueda() {
         AnchorPane anchor = new AnchorPane();
         Label mensaje = new Label("Bienvenido escoja alguna provincia");
         HBox menuTop = new HBox();
         Label provincia = new Label("Provincia");
         comboProvincia = new ComboBox();
         cargandoProvincia();
-        anchor.getChildren().addAll(mensaje,menuTop);
-        menuTop.getChildren().addAll(provincia,comboProvincia);
+        anchor.getChildren().addAll(mensaje, menuTop);
+        menuTop.getChildren().addAll(provincia, comboProvincia);
         menuTop.setSpacing(10);
-        AnchorPane.setTopAnchor(mensaje,10.0);
+        AnchorPane.setTopAnchor(mensaje, 10.0);
         AnchorPane.setTopAnchor(menuTop, 30.0);
         return anchor;
     }
-    
-    private void cargandoProvincia(){
+
+    private void cargandoProvincia() {
         try {
             comboProvincia.getItems().addAll(Provincia.leerProvincia());
+            comboProvincia.setOnAction(e -> mostrarCiudades());
         } catch (IOException e) {
-            System.err.println(e.getMessage());   
+            System.err.println(e.getMessage());
         }
+    }
+
+    private void mostrarCiudades() {
+        VBox mostrarInfo = new VBox();
+        Provincia provinEscogida = (Provincia) comboProvincia.getValue();
+        ArrayList<Ciudad> ciudades = provinEscogida.getCiudades();
+        for (Ciudad ciudade : ciudades) {
+            Label mostraP = new Label(ciudade.toString());
+            mostrarInfo.getChildren().add(mostraP);
+            //System.out.println(ciudade.toString());
+        }
+        rootGP.setCenter(mostrarInfo);
     }
 }
