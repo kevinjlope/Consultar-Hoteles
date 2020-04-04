8162ea99bc88f4fe453fb2a7d7002c1ceb450c36
Kevin Lopez <kevinjeff04@gmail.com>
2020-04-04 07:41:37 -0500

    Disenando la primera interfaz de nuestro pequeno proyecto, que es la interfaz donde el cliente ingresa por su usuario y contrasena

--- src/UI/IniciarSesion.java
+++ src/UI/IniciarSesion.java
@@ -5,10 +5,45 @@
  */
 package UI;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 /**
  *
  * @author kevjlope
  */
 public class IniciarSesion {
+    //Atributos
     
+    private VBox rootPrincipal;
+    private GridPane rootSecundario;
+    private Button iniciar;
+    
+    private VBox getRootPrincipal(){
+        return rootPrincipal;
+    }
+    
+    public IniciarSesion(){
+        rootPrincipal = new VBox();
+        rootPrincipal.getChildren().add(dibujando());
+    }
+    
+    public GridPane dibujando(){
+        rootSecundario = new GridPane();
+        TextField inputUsuario = new TextField();
+        TextField inputPass= new TextField();
+        Label usuario = new Label();
+        Label password = new Label();
+        rootSecundario.setHgap(10);
+        rootSecundario.setVgap(10);
+        rootSecundario.add(usuario, 0, 0);
+        rootSecundario.add(inputUsuario, 1, 0);
+        rootSecundario.add(password, 0, 1);
+        rootSecundario.add(inputPass, 1, 1);
+        
+        return rootSecundario;
+    }
 }
