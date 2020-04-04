ce7d6c7593af55e4927244c7024a998aea0fd74d
Kevin Lopez <kevinjeff04@gmail.com>
2020-04-04 08:02:31 -0500

    Mejorando la interfaz de iniciar sesion cuenta ahora con un boton y el tamano de letra se mejoro

--- src/UI/IniciarSesion.java
+++ src/UI/IniciarSesion.java
@@ -5,11 +5,13 @@
  */
 package UI;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 /**
  *
@@ -22,28 +24,35 @@ public class IniciarSesion {
     private GridPane rootSecundario;
     private Button iniciar;
     
-    private VBox getRootPrincipal(){
+    public VBox getRootPrincipal(){
         return rootPrincipal;
     }
     
     public IniciarSesion(){
         rootPrincipal = new VBox();
-        rootPrincipal.getChildren().add(dibujando());
+        rootPrincipal.getChildren().addAll(dibujando(),iniciar);
+        rootPrincipal.setAlignment(Pos.CENTER);
+        rootPrincipal.setSpacing(10);
+        //rootPrincipal.getChildren().add(dibujando());
     }
     
     public GridPane dibujando(){
         rootSecundario = new GridPane();
         TextField inputUsuario = new TextField();
         TextField inputPass= new TextField();
-        Label usuario = new Label();
-        Label password = new Label();
+        Label mensaje = new Label("Bienvenido!!!");
+        mensaje.setFont(new Font(30));
+        rootPrincipal.getChildren().add(mensaje);
+        Label usuario = new Label("Usuario: ");
+        Label password = new Label("Contrasena: ");
+        iniciar = new Button("Iniciar Sesion");
         rootSecundario.setHgap(10);
         rootSecundario.setVgap(10);
         rootSecundario.add(usuario, 0, 0);
         rootSecundario.add(inputUsuario, 1, 0);
         rootSecundario.add(password, 0, 1);
         rootSecundario.add(inputPass, 1, 1);
-        
+        rootSecundario.setAlignment(Pos.CENTER);
         return rootSecundario;
     }
 }
